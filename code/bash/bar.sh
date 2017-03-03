ccb8e681bee3a7ba57ecb10c3982d307f3beb83a
utsukami <geremysowers@live.com>
2017-03-02 23:35:24 -0500

    changed font size

--- code/bash/bar.sh
+++ code/bash/bar.sh
@@ -1,2 +1,2 @@
 $(pkill lemonbar);
-$(python -u ~/code/python/bar.py | lemonbar -p -g "1920x30" -B "#312e73" -u 4 -f Iosevka-16 &);
+$(python -u ~/code/python/bar.py | lemonbar -p -g "1920x30" -B "#312e73" -u 4 -f Iosevka-13 &);
