927633e609e64cfe06566e86922a8aa0eb4f3749
utsukami <geremysowers@live.com>
2017-03-16 09:05:10 -0400

    updated bar script

--- code/bash/bar.sh
+++ code/bash/bar.sh
@@ -1,3 +1,3 @@
 $(pkill lemonbar);
 $(python -u ~/code/python/bar.py | lemonbar -p -g "1920x30" -B "#312e73" -u 4 -f Iosevka-13 &);
-sh ~/code/bash/ahk_bar.sh | lemonbar -b -g "1920x5" -B "#312e73" -u 5;
+sh ~/code/bash/ahk_bar.sh | lemonbar -b -g "1920x5" -B "#312e73" -u 5 | sh;
