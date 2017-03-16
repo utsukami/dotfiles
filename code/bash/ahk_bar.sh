2ba28ee7d04e2ce49cd3d9bde6ac9be169f09535
utsukami <geremysowers@live.com>
2017-03-16 09:09:28 -0400

    made bar clickable

--- code/bash/ahk_bar.sh
+++ code/bash/ahk_bar.sh
@@ -2,14 +2,14 @@
 
 i3subscribe mode | while read -r event; do
 	if [[ $event == mode:osrs ]]; then
-		echo ' %{c}%{+u}%{U#128d8d}                %{-u} '
+		echo '%{c}%{+u}%{A1:xdotool key alt+0:}%{U#128d8d}             %{-u}%{A}'
 	elif [[ $event == mode:default ]]; then
-		echo ' %{c}%{+u}%{U#fcf17d}                %{-u} '
+		echo '%{c}%{+u}%{A1:xdotool key alt+0:}%{U#fcf17d}             %{-u}%{A}'
 	elif [[ $event == mode:resize ]]; then
-		echo ' %{c}%{+u}%{U#38c5f3}                %{-u} '
+		echo '%{c}%{+u}%{A1:xdotool key Escape:}%{U#38c5f3}             %{-u}%{A}'
 	elif [[ $event == mode:jad ]]; then
-		echo ' %{c}%{+u}%{U#e52035}                %{-u} '
+		echo '%{c}%{+u}%{U#e52035}              %{-u}'
 	else
-		echo ' %{c}%{+u}%{U#ffffff}                %{-u} '
+		echo '%{c}%{+u}%{U#ffffff}              %{-u}'
 	fi
 done
