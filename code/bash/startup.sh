af76c6575a6791b4f4d076ce575d173142db3546
utsukami <geremysowers@live.com>
2017-04-20 10:47:46 -0400

    updated to new .dunstrc path

--- code/bash/startup.sh
+++ code/bash/startup.sh
@@ -2,5 +2,5 @@ nitrogen --restore;
 xset s 600 600 && xset -b; 
 rm $HOME/.serverauth*;
 xset m 1/2 && xinput set-prop 12 283 10;
-dunst -config $HOME/.dunstrc;
+dunst -config $HOME/.config/.dunstrc;
 sh discord;
