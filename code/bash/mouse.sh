edc786f2f5f035d89f699010c4f512dde72cf61d
utsukami <geremysowers@live.com>
2017-05-03 17:47:26 -0400

    updated config files

--- code/bash/mouse.sh
+++ code/bash/mouse.sh
@@ -1,4 +1,4 @@
 for x in $(xinput | grep G502 | cut -f 2 | grep -o '[0-9]*'); do
-		$(xinput --set-prop $x $(xinput --list-props $x | cut -f 2 | grep -o 'Con.\{1,25\}' | grep -o '[0-9]*') 8)
-		$(xinput --set-prop $x $(xinput --list-props $x | cut -f 2 | grep -o 'Ada.\{1,25\}' | grep -o '[0-9]*') 8)
+		$(xinput --set-prop $x $(xinput --list-props $x | cut -f 2 | grep -o 'Con.\{1,25\}' | grep -o '[0-9]*') 10)
+		$(xinput --set-prop $x $(xinput --list-props $x | cut -f 2 | grep -o 'Ada.\{1,25\}' | grep -o '[0-9]*') 10)
 done
