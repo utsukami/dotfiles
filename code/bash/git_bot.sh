187a6cb761b5be88c7ccabbd4d760ed614ec81df
utsukami <geremysowers@live.com>
2017-11-29 14:43:30 -0500

    cleaned up a lot of the code (shoutout to deathbeam)

--- code/bash/git_bot.sh
+++ code/bash/git_bot.sh
@@ -1,28 +1,28 @@
 setxkbmap dvorak; 
-files=~/code/bash/.git_bot_files
 
-for phrase in $(grep -oP '(?<=[Gg]it )..[A-Za-z0-9]*' $files/git_listener | tail -n 1); do
-	phrase_removed=$(echo "$phrase" | tr -d '-');
+files=~/code/bash/.git_bot_files;
+githelp=$(git --help);
+phrase=$(grep -oP '(?<=[Gg]it )..[A-Za-z0-9]*' $files/git_listener | tail -n 1);
 
-	if grep -q "$phrase_removed" $files/git_comms $files/git_args; then	
-		if [[ "$phrase" == "--help" ]]; then
-			for (( x=1; x<=$(git --help | sed '/^\s*$/d' | wc -l); x++ )); do
-				gitout=$(git --help | sed '/^\s*$/d' | head -n $x | tail -n 1);
+if grep -q "$phrase" $files/git_comms $files/git_args > /dev/null; then	
+	if [[ "$phrase" == "--help" ]]; then
+		for (( x=1; x<=$(git --help | sed '/^\s*$/d' | wc -l); x++ )); do
+			gitout=$(echo $githelp | sed '/^\s*$/d' | head -n $x | tail -n 1);
 
-				if [[ $x == 31 ]]; then
-					sleep 1.5; xdotool type --delay 50 "/$(git --help | sed '/^\s*$/d' | head -n $x | tail -n 1 | head --bytes -15)";
-					xdotool type --delay 50 "$(git --help | sed '/^\s*$/d' | head -n $x | tail -n 1 | tail --bytes -13)";
-					xdotool key Return;
-				else
-					sleep .4; xdotool type --delay 50 "/$gitout";
-					xdotool key Return;
-					sleep 1.5;
-				fi;
-			done;
-		else
-			:
-		fi;
+			if [[ $x == 31 ]]; then
+				sleep 1.5; xdotool type --delay 50 "/$(echo $gitout | head --bytes -15)";
+				xdotool type --delay 50 "$(echo $gitout | tail --bytes -13)";
+				xdotool key Return;
+			else
+				sleep .4; xdotool type --delay 50 "/$gitout";
+				xdotool key Return;
+				sleep 1.5;
+			fi;
+		done;
 	else
-		sleep .5; xdotool type --delay 20 "/git: '$phrase' is not a git command. See 'git --help'." && sleep 0.3; xdotool key Return;
+		:
 	fi;
-done;
+else
+	sleep .5; xdotool type --delay 20 "/git: '$phrase' is not a git command. See 'git --help'." && sleep 0.3; 
+	xdotool key Return;
+fi;
