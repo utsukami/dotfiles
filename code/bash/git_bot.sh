ef264b752158e61fcc85aaf7647323184ed0fdd6
utsukami <geremysowers@live.com>
2017-11-29 17:02:23 -0500

    another update

--- code/bash/git_bot.sh
+++ code/bash/git_bot.sh
@@ -1,28 +1,22 @@
-setxkbmap dvorak; 
+setxkbmap dvorak;
 
 files=~/code/bash/.git_bot_files;
 githelp=$(git --help);
 phrase=$(grep -oP '(?<=[Gg]it )..[A-Za-z0-9]*' $files/git_listener | tail -n 1);
+i=$(expr $(echo $githelp | wc -m) / 43);
+h=43;
 
-if grep -q "$phrase" $files/git_comms $files/git_args > /dev/null; then	
+if grep -q "$phrase" $files/git_comms $files/git_args > /dev/null; then
 	if [[ "$phrase" == "--help" ]]; then
-		for (( x=1; x<=$(git --help | sed '/^\s*$/d' | wc -l); x++ )); do
-			gitout=$(echo $githelp | sed '/^\s*$/d' | head -n $x | tail -n 1);
-
-			if [[ $x == 31 ]]; then
-				sleep 1.5; xdotool type --delay 50 "/$(echo $gitout | head --bytes -15)";
-				xdotool type --delay 50 "$(echo $gitout | tail --bytes -13)";
-				xdotool key Return;
-			else
-				sleep .4; xdotool type --delay 50 "/$gitout";
-				xdotool key Return;
-				sleep 1.5;
-			fi;
+		for (( x=1; x<=$i; x++ )); do
+			sleep .2; xdotool type --delay 10 "/$(echo $githelp | head --bytes $h | tail --bytes 43)";
+			xdotool key Return;
+			(( h += 43 ));
 		done;
 	else
 		:
 	fi;
 else
-	sleep .5; xdotool type --delay 20 "/git: '$phrase' is not a git command. See 'git --help'." && sleep 0.3; 
+	sleep .5; xdotool --delay 20 "/git: '$phrase' is not a git command. See 'git --help'.";
 	xdotool key Return;
 fi;
