09d2a4c82a33483a24e9bd10687e37e2141b632a
utsukami <geremysowers@live.com>
2017-11-29 17:06:43 -0500

    fixed sleep timers

--- code/bash/git_bot.sh
+++ code/bash/git_bot.sh
@@ -9,14 +9,14 @@ h=43;
 if grep -q "$phrase" $files/git_comms $files/git_args > /dev/null; then
 	if [[ "$phrase" == "--help" ]]; then
 		for (( x=1; x<=$i; x++ )); do
-			sleep .2; xdotool type --delay 10 "/$(echo $githelp | head --bytes $h | tail --bytes 43)";
-			xdotool key Return;
+			sleep .3; xdotool type --delay 50 "/$(echo $githelp | head --bytes $h | tail --bytes 43)";
+			xdotool key Return; sleep 1.2;
 			(( h += 43 ));
 		done;
 	else
 		:
 	fi;
 else
-	sleep .5; xdotool --delay 20 "/git: '$phrase' is not a git command. See 'git --help'.";
+	sleep .3; xdotool type --delay 35 "/git: '$phrase' is not a git command. See 'git --help'.";
 	xdotool key Return;
 fi;
