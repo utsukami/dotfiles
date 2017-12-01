6709f8f218f103c437efc5a51702b7ddff11eb4f
utsukami <geremysowers@live.com>
2017-12-01 13:57:39 -0500

    small change, will improve later

--- code/bash/bar.sh
+++ code/bash/bar.sh
@@ -1,3 +1,3 @@
-kill $(pgrep -f 'lemonbar') $(pgrep -f 'ahk_bar') $(pgrep -f 'i3subscribe');
+kill $(pgrep -f 'lemonbar') $(pgrep -f 'mode_bar') $(pgrep -f 'i3subscribe');
 $(python -u ~/code/python/bar.py | lemonbar -p -g "1920x30" -B "#00000000" -u 4 -f Iosevka-13 &);
-sh ~/code/bash/ahk_bar.sh | lemonbar -b -g "1920x5" -B "#00000000" -u 5 | sh;
+sh ~/code/bash/mode_bar.sh | lemonbar -b -g "1920x5" -B "#00000000" -u 5 | sh;
