f5ce97d59f40a85eadcd2f1ec25a5c75308168e9
utsukami <geremysowers@live.com>
2017-12-16 22:19:12 -0500

    removing old layout

--- code/bash/lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TMPBG=$HOME/code/bash/.tmp/screen.png
-TMPBG=~/code/bash/.tmp/screen.png
-scrot ~/code/bash/.tmp/screen.png
-convert $TMPBG -scale 10% -scale 1000% $TMPBG
-convert $TMPBG -gravity center -composite -matte $TMPBG
-i3lock -i $TMPBG
