8b1551139b6e302b044f296665f8e15d9a3eb8a0
utsukami <geremysowers@live.com>
2020-10-22 23:10:40 -0400

    Added menu generator

--- code/python/ping_rs.py
+++ code/python/ping_rs.py
@@ -10,8 +10,51 @@ from subprocess import Popen, PIPE
 from sys import platform
 from time import time
 
-home = expanduser("~")
-file_save = Path("{}/.osrs_worlds.html".format(home))
+file_save = Path("{}/.osrs_worlds.html".format(expanduser("~")))
+main_menu = ("All", "Region", "Specific")
+regions = (
+    "United States (East)",
+    "United States (West)",
+    "United Kingdom",
+    "Germany",
+    "Australia"
+)
+
+
+def file_get(method, statement, action):
+    http = Http()
+    status, response = http.request(
+        "http://oldschool.runescape.com/slu?order=WLMPA"
+    )
+    data = BeautifulSoup(response, "lxml")
+
+    with open(file_save, method) as f:
+        for lines in data.find_all("tr"):
+            f.write(str(lines))
+
+    print("\nFile:   {}\nAction: {}\n".format(statement, action))
+
+
+def menu_construct(menu, whatis, lister, updated):
+    full_string = "\nSelect:\n - {}:\n\n\t".format(whatis)
+
+    for entries in range(1, menu + 1):
+        if entries + 1 == menu + 1:
+            if updated:
+                full_string += "{}] {}".format(entries, lister[entries - 1])
+
+            else:
+                full_string += "{}] {}\n\n".format(entries, lister[entries - 1])
+
+        else:
+            full_string += "{}] {}\n\t".format(entries, lister[entries - 1])
+
+    if not updated:
+        full_string += " - Options:\n\n\t4] Update world list"
+
+    full_string += "\n\nEnter: "
+
+    return full_string
 
 
 def ping_command(world_url, ping_count):
@@ -52,11 +95,9 @@ def ping_command(world_url, ping_count):
     return min_ms, max_ms, avg_ms
 
 
-def parse_html_data(reply, region_choice, specific_world):
+def start_ping(reply, region_choice, specific_world):
     with open(file_save) as file_contents:
         worlds = BeautifulSoup(file_contents, "lxml")
-
-    recommend = {}
 
     if 3 >= reply > 0:
         for info in worlds.find_all("tr"):
@@ -71,28 +112,14 @@ def parse_html_data(reply, region_choice, specific_world):
                     url = url.replace(" ", "").lower()
                     full_title = number_filter + ": " + title
 
-                    if reply == 1:
-                        start_ping = ping_command(url, 1)
-
-                        print(number_filter + ": ", str(start_ping[0]) + " MS")
-                        recommend[full_title] = int(start_ping[0])
-
-                    elif (reply == 2 and
-                            region_choice in loc.get_text()):
-
-                        start_ping = ping_command(url, 1)
-
-                        print(number_filter + ": ", str(start_ping[0]) + " MS")
-                        recommend[full_title] = int(start_ping[0])
-
-                    elif reply == 3:
+                    if reply == 3:
                         if specific_world in number_filter:
                             print(
                                 "\nChecking ping on world {}..\n"
                                 .format(number_filter)
                             )
 
-                            start_ping = ping_command(url, 15)
+                            start_ping = ping_command(url, 20)
 
                             print(
                                 full_title + "\n",
@@ -102,69 +129,42 @@ def parse_html_data(reply, region_choice, specific_world):
                                 )
                             )
 
-    if recommend:
-        print("\nRecommended worlds:")
-        for lowest in range(1, 4):
-            get_recommend = min(recommend.items(), key=itemgetter(1))
-            print("\n  ", get_recommend[0], "\n    ", get_recommend[1], " MS")
-            recommend.pop(get_recommend[0])
-        print("\r")
+                    else:
+                        start_ping = ping_command(url, 1)
+
+                        if reply == 1:
+                            print(number_filter + ": ", str(start_ping[0]) + " MS")
+
+                        if (reply == 2 and
+                                region_choice in loc.get_text()):
+
+                            if specific_world:
+                                if (specific_world == 1 and
+                                        int(start_ping[0]) < 75):
+
+                                    print(number_filter + ": ", str(start_ping[0]) + " MS")
+
+                                elif (specific_world == 2 and
+                                        int(start_ping[0]) > 75):
+
+                                    print(number_filter + ": ", str(start_ping[0]) + " MS")
+
+                            else:
+                                    print(number_filter + ": ", str(start_ping[0]) + " MS")
 
     if platform == "win32":
         input("Hit Return to exit.")
 
 
-def file_get(method, statement, action):
-    http = Http()
-    status, response = http.request(
-        "http://oldschool.runescape.com/slu?order=WLMPA"
-    )
-    data = BeautifulSoup(response, "lxml")
-
-    with open(file_save, method) as f:
-        for lines in data.find_all("tr"):
-            f.write(str(lines))
-
-    print("\nFile:   {}\nAction: {}\n".format(statement, action))
-
-
 def selector(selection):
-    regions = ("United States", "United Kingdom", "Germany", "Australia")
-
     try:
-        if selection:
-            get_select = int(
-                input(
-                    "Select:\n - Ping types:\n\n\t1] {}"
-                    "2] {}3] {} - Options:\n\n\t4] {}"
-                    .format(
-                        "All\n\t", "Region\n\t", "Specific\n\n",
-                        "Update world list\n\nEnter: "
-                    )
-                )
-            )
-
-        else:
-            get_select = int(
-                input(
-                    "Select:\n Ping types:\n\n\t1] All \n\t2] Region "
-                    "\n\t3] Specific\n\nEnter: "
-                )
-            )
-
-        if get_select == 1:
+        if selection == 1:
             print("\nChecking ping on every world..\n")
-            parse_html_data(get_select, None, None)
+            start_ping(selection, None, None)
 
-        elif get_select == 2:
+        elif selection == 2:
             region_select = int(
-                input(
-                    "\nSelect:\n - Region:\n\n\t1] {}2] {}3] {}4] {}\nEnter: "
-                    .format(
-                        regions[0] + "\n\t", regions[1] + "\n\t",
-                        regions[2] + "\n\t", regions[3] + "\n\t"
-                    )
-                )
+                input(menu_construct(len(regions), "Region", regions, 1))
             )
 
             if len(regions) >= region_select >= 1:
@@ -173,15 +173,24 @@ def selector(selection):
                     .format(regions[region_select - 1])
                 )
 
-                parse_html_data(get_select, regions[region_select - 1], None)
+                if (region_select == 1 or
+                        region_select == 2):
 
-        elif get_select == 3:
+                    strip_coast = regions[0][:regions[0].index("(") - 1]
+                    start_ping(selection, strip_coast, region_select)
+
+                else:
+                    start_ping(selection, regions[region_select - 1], None)
+
+        elif selection == 3:
             world_num = input("\nEnter world number: ")
-            parse_html_data(get_select, None, world_num)
+            start_ping(selection, None, world_num)
 
-        elif get_select == 4:
+        elif selection == 4:
             file_get("w+", "[OUTDATED]" ,"[UPDATE]")
-            selector(None)
+            selector(int(input(menu_construct(
+                len(main_menu), "Ping types", main_menu, 1)))
+            )
 
     except ValueError:
         pass
@@ -193,14 +202,20 @@ try:
 
         if last_updated > 604800:
             file_get("w+", "[OUTDATED]", "[UPDATE]")
-            selector(None)
+            selector(int(input(menu_construct(
+                len(main_menu), "Ping types", main_menu, 1)))
+            )
 
         else:
-            selector(True)
+            selector(int(input(menu_construct(
+                len(main_menu), "Ping types", main_menu, 0)))
+            )
 
     else:
         file_get("a+", "[NOT FOUND]", "[CREATE]")
-        selector(None)
+        selector(int(input(menu_construct(
+            len(main_menu), "Ping types", main_menu, 1)))
+        )
 
 except KeyboardInterrupt:
     pass
